45b53acfb89c0b508d73defe3ac07dc194d60f60
Mark Nair <47827647+marknair@users.noreply.github.com>
2024-10-31 21:48:50 -0500

    Initial Commit

---
+++ Random Chooser/Random_ChooserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_ChooserApp.swift
+//  Random Chooser
+//
+//  Created by Mark Nair on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Random_ChooserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
